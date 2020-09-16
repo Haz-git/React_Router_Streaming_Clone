69373bbf198f84b93fe2db0d064eaad3a09172ec
Haz-git <zhouharry1@gmail.com>
2020-09-16 12:01:26 -0700

    Linked redux-form with StreamCreate component

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, composeEnhancers(applyMiddleware()))}>
         <App />
     </Provider>,
     document.getElementById('root')
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; //pre-made reducer from pkg
 import authReducer from './authReducers';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer //specific key necessary
 })
