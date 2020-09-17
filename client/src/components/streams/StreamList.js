ee0afc4f9637d88c8aa47e9511f749d8200aa9e2
Haz-git <zhouharry1@gmail.com>
2020-09-17 10:43:26 -0700

    Added Create Stream Button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends Component {
 
@@ -38,11 +39,25 @@ class StreamList extends Component {
         })
     }
 
+    //Renders a create stream button when user is signed in using the isSignedIn boolean in auth.
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to='/streams/new' className='ui button primary'>
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Streams Availiable</h2>
                 <div className='ui celled list'>{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         )
     }
@@ -51,7 +66,9 @@ class StreamList extends Component {
 const mapStateToProps = state => {
      return {
          streams: Object.values(state.streams),
-         currentUserId: state.auth.userId
+         currentUserId: state.auth.userId,
+         isSignedIn: state.auth.isSignedIn
+
      }
 }
 
