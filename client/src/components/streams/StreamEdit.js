55ca464ea9bfc4eb53054f6876e0f4ff5330b895
Haz-git <zhouharry1@gmail.com>
2020-09-18 05:41:38 -0700

    Created StreamForm component to increase reuse

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-    return (
-        <div>
-            StreamEdit
-        </div>
-    )
+class StreamEdit extends React.Component {
+    //Making StreamEdit work in isolation:
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+
+    render() {
+        //This conditional is necessary because this.props.stream.title is not yet loaded on load.
+        if (!this.props.stream) {
+            return <div>Loading....</div>
+        }
+
+        return (
+            <div>
+                {this.props.stream.title}
+            </div>
+        )
+    }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps)
+    return {
+        //Select appropriate stream out of props
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
